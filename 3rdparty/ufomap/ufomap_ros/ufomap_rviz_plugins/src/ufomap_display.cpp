a180b4129efe8fa3b27e877cb3417d1189b622ee
hjl <hjlstudy@163.com>
2024-07-17 18:47:48 +0800

    修复UFOmap_rviz_plugin编译问题

--- 3rdparty/ufomap/ufomap_ros/ufomap_rviz_plugins/src/ufomap_display.cpp
+++ 3rdparty/ufomap/ufomap_ros/ufomap_rviz_plugins/src/ufomap_display.cpp
@@ -77,15 +77,15 @@ void UFOMapDisplay::onInitialize()
 
 	info_property_ = new rviz::Property("Information", QVariant(), "", this);
 	resolution_property_ = new rviz::StringProperty(
-	    "Resolution", "", "Resolution of the occupancy map", info_property_, SLOT(info_property_), this);
+	    "Resolution", "", "Resolution of the occupancy map", info_property_);
 	num_leaf_nodes_property_ =
 	    new rviz::StringProperty("# Leaf Nodes", "", "Number of leaf nodes in the octree",
-	                             info_property_, SLOT(info_property_), this);
+	                             info_property_);
 	num_inner_nodes_property_ =
 	    new rviz::StringProperty("# Inner Nodes", "", "Number of inner nodes in the octree",
-	                             info_property_, SLOT(info_property_), this);
+	                             info_property_);
 	size_property_ = new rviz::StringProperty("Size", "", "Size of the octree",
-	                                          info_property_, SLOT(info_property_), this);
+	                                          info_property_);
 
 	render_category_property_ = new rviz::Property("Voxel Rendering", QVariant(), "", this);
 	for (VoxelType const& type : {OCCUPIED, FREE, UNKNOWN}) {
